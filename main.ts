2e8f550805488d2587da4955762a0355df799973
zcodery <1570241334@qq.com>
2025-12-01 09:41:29 +0800

    feat: 添加 element-ui 依赖并初始化配置
    在项目中添加 element-ui 组件库依赖，并在入口文件中引入其样式和主模块，以支持 UI 组件功能

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 const params = new URLSearchParams(window.location.search)
 const readonlyParam = params.get('readonly')
