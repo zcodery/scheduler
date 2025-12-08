62bc05a1859eae4b1c5f17d8a1dd86ff9ab4cb5d
zcodery <1570241334@qq.com>
2025-12-08 13:45:50 +0800

    feat: 添加Vue插件安装功能并更新版本号
    添加ResourceSchedulerPlugin插件安装功能，支持全局注册组件
    默认导出GanttScheduler组件，同时保持原有导出方式
    更新package.json版本号至0.1.11

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-export { default as GanttScheduler } from "./common/gantt-scheduler.vue"
+import GanttScheduler from "./common/gantt-scheduler.vue"
+export { GanttScheduler }
 export * from "./types"
 export * from "./utils/constants"
+
+export const ResourceSchedulerPlugin = {
+  install(Vue: any) {
+    if (Vue && typeof Vue.component === "function") {
+      Vue.component("gantt-scheduler", GanttScheduler)
+    }
+  },
+}
+
+export default GanttScheduler
